8bdea375e3d941d581b71056fcd62083a146fa84
Maya Ashizumi-Munn <maya.ashizumimunn@asknice.ly>
2025-01-08 11:54:09 +1300

    WIP modal

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,10 +1,21 @@
 <template>
     <div>
         Done It App
+        <button @click="openDoneItModal">Create new Done It</button>
+
+        <NewDoneItModal v-if="modalOpen" />
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue"
+import NewDoneItModal from "./components/NewDoneItModal.vue";
+
+const modalOpen = ref(false)
+
+const openDoneItModal = () => {
+    modalOpen.value = true
+}
 </script>
 
 <style scoped>
---
+++ vue-app/src/components/NewDoneItModal.vue
@@ -0,0 +1,18 @@
+<template>
+    <dialog class="done-it-modal">
+        <label for="desc">Done It Description</label>
+        <input type="text" name="desc" />
+    </dialog>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="less" scoped>
+.done-it-modal {
+    border: 1px solid black;
+    height: 600px;
+    width: 400px;
+}
+</style>
