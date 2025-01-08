a640437f1ba923b91bf88657d2ca689a5a255e48
Maya Ashizumi-Munn <maya.ashizumimunn@asknice.ly>
2025-01-08 17:25:24 +1300

    Create category select

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# done-it-list
+# Done it list
 Done It and ToDo list app to track daily tasks and categorise how I spend my time during the work day
+
+### Upcoming features
+- Todo list where todos can be converted to done its
+- Calendar view and list view of done its
+- Reporting for categories/productivity etc
+- Ability to set day off to exclude from reporting
+- Jira API to show reporting of sprint productivity
--- vue-app/src/components/NewDoneItModal.vue
+++ vue-app/src/components/NewDoneItModal.vue
@@ -1,13 +1,12 @@
 <template>
     <Dialog v-model:visible="visible" modal header="Create new Done It" id="done-it-modal">
         <FloatLabel variant="on" class="float-label">
-            <label for="desc">Description</label>
             <InputText id="desc" class="float-label__text-input" v-model="description" />
+            <label for="desc">Description</label>
         </FloatLabel>
 
         <div class="time-pickers">
             <FloatLabel variant="on" class="float-label">
-                <label for="start-time">Start Time</label>
                 <DatePicker
                     v-model="startTime"
                     id="start-time"
@@ -15,10 +14,10 @@
                     hourFormat="12"
                     fluid
                 />
+                <label for="start-time">Start Time</label>
             </FloatLabel>
 
             <FloatLabel variant="on" class="float-label">
-                <label for="end-time">End Time</label>
                 <DatePicker
                     v-model="endTime"
                     id="end-time"
@@ -26,9 +25,14 @@
                     hourFormat="12"
                     fluid
                 />
+                <label for="end-time">End Time</label>
             </FloatLabel>
         </div>
 
+        <FloatLabel variant="on" class="float-label">
+            <Select v-model="category" :options="categories" input-id="cat-select" option-label="name" class="float-label__select-input" />
+            <label for="cat-select">Category</label>
+        </FloatLabel>
 
         <Button @click="createDoneIt" severity="success" id="create-button">
             Create
@@ -38,14 +42,27 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { Dialog, FloatLabel, InputText, Button, DatePicker } from "primevue"
+import { Dialog, FloatLabel, InputText, Button, DatePicker, Select } from "primevue"
 
 const visible = defineModel()
 const emit = defineEmits(['close'])
 
+interface Category {
+    name: string
+    color: string
+}
+const categories: Category[] = [
+    { name: 'Sprint ticket', color: '#3446eb' },
+    { name: 'PR', color: '#7b7c85' },
+    { name: 'Meeting', color: '#4cc271' },
+    { name: 'Infrastructure', color: '#944cc2' },
+    { name: 'Initiative', color: '#c24cba' },
+]
+
 const description = ref('')
 const startTime = ref()
 const endTime = ref('-')
+const category = ref()
 
 const createDoneIt = () => {
     emit('close')
@@ -61,6 +78,10 @@ const createDoneIt = () => {
 
     &__text-input {
         width: 600px;
+    }
+
+    &__select-input {
+        width: 100%;
     }
 }
 
