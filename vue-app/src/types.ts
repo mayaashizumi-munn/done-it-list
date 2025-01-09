86ce623299ca7fecbe835675597791c4d986611e
paradiseitnz <maya.github@protonmail.com>
2025-01-09 22:54:28 +1300

    Create new types for DoneIt and DoneItDate

---
+++ vue-app/src/types.ts
@@ -0,0 +1,14 @@
+export interface DoneIt {
+    id: number
+    title: string
+    description?: string
+    startTime: Date
+    endTime?: Date
+    categoryType: string
+    categoryLabel: string
+}
+
+export interface DoneItDate {
+    date: string
+    doneIts: DoneIt[]
+}
