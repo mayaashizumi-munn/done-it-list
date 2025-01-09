a5e678224bd44d612fc6b9b359d2c5a8ee4f17f8
paradiseitnz <maya.github@protonmail.com>
2025-01-09 22:55:15 +1300

    Render done it list and style it

--- vue-app/src/components/DoneItList.vue
+++ vue-app/src/components/DoneItList.vue
@@ -1,25 +1,88 @@
 <template>
-    <div class="list">
-        <div class="done-it-list-item" v-for="doneit in list" :key="doneit.id">
-            {{  doneit }}
+    <div v-if="!loading && props.doneIts?.length" id="done-it-dates-list">
+        <div class="done-it-date" v-for="date in organisedByDate" :key="date.date">
+            {{ date.date }}
+
+            <div class="done-it-item" v-for="doneIt in date.doneIts">
+                <span class="done-it-item-section__time">
+                    {{ getFormattedTime(doneIt.startTime) }}
+                    <span v-if="doneIt.endTime">
+                        - {{ getFormattedTime(doneIt.endTime) }}
+                    </span>
+                </span>
+                
+                <span class="done-it-item-section__category">
+                    <CategoryPill :type="doneIt.categoryType" :label="doneIt.categoryLabel" />
+                </span>
+
+                <span>
+                    {{ doneIt.title }}
+                </span>
+                <span v-if="doneIt.description">
+                    {{ doneIt.description }}
+                </span>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { onBeforeMount, ref } from 'vue'
-import { getAllFromDb } from '../idb'
+import { computed, type ComputedRef } from 'vue'
+import type { DoneIt, DoneItDate } from '../types'
+import CategoryPill from './CategoryPill.vue'
 
-const list = ref()
+interface Props {
+    loading: boolean
+    doneIts: DoneIt[]
+}
+const props = defineProps<Props>()
 
-onBeforeMount(async () => {
-    list.value = await getAllFromDb('doneit')
+const organisedByDate: ComputedRef<DoneItDate[]> = computed(() => {
+    return props.doneIts?.reduce((result: DoneItDate[], item) => {
+        const date = item.startTime?.toDateString()
+        const existingGroup = result.find(group => group.date === date)
+
+        if (existingGroup) {
+            existingGroup.doneIts.push(item)
+        } else {
+            result.push({ date, doneIts: [item] })
+        }
+
+        return result
+    }, [])
 })
+
+const getFormattedTime = (date: Date) => {
+    return date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+}
 </script>
 
 <style lang="less" scoped>
-.done-it-list-item {
+#done-it-dates-list {
+    width: 100%;
+}
+
+.done-it-date {
     width: 100%;
     text-align: start;
 }
+
+.done-it-item {
+    border: 1px solid black;
+    display: flex;
+    width: 100%;
+}
+
+.done-it-item-section {
+    &__time {
+        width: 90px;
+    }
+
+    &__category {
+        display: flex;
+        align-items: center;
+        padding: 0 8px;
+    }
+}
+
 </style>
