49e17bad1218e47b0a6bc07d59b48487bc599638
Maya Ashizumi-Munn <maya.github@protonmail.com>
2025-01-15 21:49:28 +1300

    Set up prime icons in main.ts

--- vue-app/src/main.ts
+++ vue-app/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+import './style.css'
+import 'primeicons/primeicons.css'
 
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
