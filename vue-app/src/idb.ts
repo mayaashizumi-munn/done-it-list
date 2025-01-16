ab3e208ae73e5fdf546920d94c6f099953a2c384
Maya Ashizumi-Munn <maya.github@protonmail.com>
2025-01-16 22:41:35 +1300

    Make changes to idb to accomodate for new stores

--- vue-app/src/idb.ts
+++ vue-app/src/idb.ts
@@ -1,31 +1,38 @@
 import { openDB, type IDBPDatabase } from 'idb'
+import { DONE_IT_DB, TODO_DB } from './constants'
 
 const dbName = 'doneitdb'
-const dbVer = 1
+const dbVer = 3
 
-async function setupDB(storeName: string): Promise<IDBPDatabase<unknown>> {
+const storeNames = [TODO_DB, DONE_IT_DB]
+
+async function setupDB(): Promise<IDBPDatabase<unknown>> {
     // Create or open DB
     return await openDB(dbName, dbVer, {
         upgrade(db) {
             // Create a new object store if it doesn't already exist
-            if (!db.objectStoreNames.contains(storeName)) {
-                db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+            for (const store of storeNames) {
+                if (!db.objectStoreNames.contains(store)) {
+                    db.createObjectStore(store, { 
+                        keyPath: 'id', autoIncrement: true 
+                    });
+                }
             }
         }
     })
 }
 
 export async function addToDb(storeName: string, data: any): Promise<IDBValidKey> {
-    const db = await setupDB(storeName)
+    const db = await setupDB()
     return db.add(storeName, data)
 }
 
 export async function getAllFromDb(storeName: string): Promise<any[]> {
-    const db = await setupDB(storeName)
+    const db = await setupDB()
     return db.getAll(storeName)
 }
 
 export async function deleteFromDb(storeName: string, id: number): Promise<void> {
-    const db = await setupDB(storeName)
+    const db = await setupDB()
     return db.delete(storeName, id)
 }
